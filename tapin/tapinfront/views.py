9fb11a4660eb03afd0d69a2087cefffb77de9a6d
William Zhang <willzhang05@gmail.com>
2018-03-10 18:52:26 -0500

    add initial page

--- tapin/tapinfront/views.py
+++ tapin/tapinfront/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse, HttpResponseBadRequest
+
 # Create your views here.
-def index():
-    return 'hi'
+
+def index(request):
+    return render(request, 'index.html')
