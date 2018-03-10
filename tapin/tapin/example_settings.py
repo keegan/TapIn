6ffbc9202ff3c259e690b39b24e47ef8fda2dc9d
William Zhang <willzhang05@gmail.com>
2018-03-10 18:44:32 -0500

    add tapinfront

--- tapin/tapin/example_settings.py
+++ tapin/tapin/example_settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qpvzn7)+tv5*u9uo3s4ak6ofu_ta=ye#a(4lod@p*__h#v8%(n'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
---
+++ tapin/tapinfront/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TapinfrontConfig(AppConfig):
+    name = 'tapinfront'
