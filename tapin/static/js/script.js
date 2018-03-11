3f4775f818567e135f9f41a2f684668c5ecfa6b6
William Zhang <willzhang05@gmail.com>
2018-03-10 23:48:00 -0500

    fix views for tapinback

--- tapin/static/js/script.js
+++ tapin/static/js/script.js
@@ -1,17 +1,18 @@
 "use strict"
 
 function pollBackend(output) {
-    var request = new xmlHttpRequest();
-    request.open("GET", "/backend/status", true);
-    request.send(null)
-    res = request.responseText;
+    var request = new XMLHttpRequest();
+    request.open("GET", "/backend/status", false);
+    request.send(null);
+    var res = request.responseText;
+    console.log("hello: " + res);
     output = JSON.parse(res);
 }
     
 
 
 window.onload = function() {
-    output = { "status": "", "username": ""};
+    var output = { "status": "", "username": ""};
     setInterval( function() { pollBackend(output); }, 500 );
-    console.log(output)
+    console.log(output);
 }
--- tapin/tapinback/views.py
+++ tapin/tapinback/views.py
@@ -1,16 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 
-from .forms import AuthForm
+import json
+import random
+
+def status(request):
+    if request.method == 'GET':
+
+        res = {}
+        res['status'] = 'in progress' if random.random() < 0.8 else 'success'
+        res['username'] = '2018wzhang'
+        return HttpResponse(json.dumps(res), content_type='application/json')
+    else:
+        return HttpResponse(status=400)
+
+#from .forms import AuthForm
 # Create your views here.
-def formRequest(request): 
-	if request.method == "POST":
-		form = AuthForm(request.POST);
-		if form.isValid():
+#def formRequest(request): 
+	#if request.method == "POST":
+		#form = AuthForm(request.POST)
+		#if form.isValid():
 			#process data
 			#return to another url v
 			#return HTTPResponseDirect(insert url here)
-	else:
-		form = AuthForm()
+	#else:
+	#	form = AuthForm()
 
-	return render(request, "backui.html", {'form':form})
+	#return render(request, "backui.html", {'form':form})
