3d19b6ea8d91af3bc2031121c3d7a9532065fd99
Keegan Lanzillotta <swfanboy01@gmail.com>
2018-03-10 23:56:41 -0500

    Models

--- tapin/tapinback/models.py
+++ tapin/tapinback/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 import secrets
+import uuid
 
 class TapUser(models.Model):
     token = models.BinaryField(default=secrets.token_bytes(896))
     pin = models.CharField(max_length=6)
     id = models.UUIDField(default=uuid.uuid4, primary_key=True)
-    userid = models.ForeignKey(User)
+    userid = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
