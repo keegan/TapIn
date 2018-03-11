a09bd6122b05212298e0b18884c8b2ba0767964c
William Zhang <willzhang05@gmail.com>
2018-03-11 04:25:23 -0400

    many frontend fixes

--- tapin/static/js/script.js
+++ tapin/static/js/script.js
@@ -1,20 +1,29 @@
 "use strict"
 
+let hostname = "pi3-curie";
+
 function pollBackend(output) {
     var request = new XMLHttpRequest();
-    request.open("GET", "/backend/status", true);
+    request.open("GET", "/backend/status?hostname=" + hostname, true);
     request.onreadystatechange = function (e) {
         if (request.readyState === 4 && request.status === 200) { 
             var res = request.responseText;
             console.log("hello: " + res);
             output = JSON.parse(res);
-            if("token" in output && "username" in output) {
-                console.log(output.token);
-                console.log("ready");
-                var tokenInput = document.querySelector("#login > .token");
-                tokenInput.value = output.token;
-                var usernameInput = document.querySelector("#login > .username");
-                usernameInput.value = output.username;
+            if (output.status === "success") {
+                if ("token" in output && "username" in output) {
+                    console.log(output.token);
+                    console.log("ready");
+                    var tokenInput = document.querySelector("#login > .token");
+                    tokenInput.value = output.token;
+                    var usernameInput = document.querySelector("span#username");
+                    usernameInput.innerHTML = "You are " + output.username;
+                    var loginForm = document.querySelector("#login");
+                    loginForm.style.visibility = "visible";
+                    loginForm.style.opacity = 1;
+                }
+            } else {
+                setTimeout(pollBackend(output), 500);
             }
         }
     };
@@ -25,8 +34,7 @@ function pollBackend(output) {
 }
     
 
-
 window.onload = function() {
     var output = { "status": "", "username": ""};
-    var pollingInterval = setInterval( function() { setTimeout(function() {pollBackend(output);}, 500);}, 500);
+    pollBackend(output);
 }
