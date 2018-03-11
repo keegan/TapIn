41f4f581037da7db77f820113e25c74359a57dca
Keegan Lanzillotta <swfanboy01@gmail.com>
2018-03-10 23:15:03 -0500

    Backend Model

--- tapin/tapinback/models.py
+++ tapin/tapinback/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+import secrets
 
-# Create your models here.
+class TapUser(models.Model):
+    token = models.BinaryField(default=secrets.token_bytes(896))
+    pin = models.CharField(max_length=6)
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True)
+    userid = models.ForeignKey(User)
