4e15616d178fb93d7344a0e4fa04aad66b95bb50
William Zhang <willzhang05@gmail.com>
2018-03-11 05:21:42 -0400

    add js token

--- tapin/static/js/script.js
+++ tapin/static/js/script.js
@@ -1,10 +1,14 @@
 "use strict"
 
-let hostname = "pi3-curie";
+const hostname = "pi3-curie";
+const url = new URL(window.location.href);
+const params = new URLSearchParams(url.search);
+const token = params.get('token')
 
 function pollBackend(output) {
     var request = new XMLHttpRequest();
-    request.open("GET", "/backend/status?hostname=" + hostname, true);
+    
+    request.open("GET", "/backend/status?hostname=" + hostname + "&token=" + token, true);
     request.onreadystatechange = function (e) {
         if (request.readyState === 4 && request.status === 200) { 
             var res = request.responseText;
@@ -16,7 +20,7 @@ function pollBackend(output) {
                     console.log("ready");
                     var tokenInput = document.querySelector("#login > .token");
                     tokenInput.value = output.token;
-                    var usernameInput = document.querySelector("span#username");
+                    var usernameInput = document.querySelector("h1#status");
                     usernameInput.innerHTML = "You are " + output.username;
                     var loginForm = document.querySelector("#login");
                     loginForm.style.visibility = "visible";
