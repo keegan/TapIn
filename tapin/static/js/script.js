74606d9faf543e0617252e74c251004451c73b52
William Zhang <willzhang05@gmail.com>
2018-03-11 01:37:51 -0500

    requests without jquery

--- tapin/static/js/script.js
+++ tapin/static/js/script.js
@@ -2,12 +2,26 @@
 
 function pollBackend(output) {
     var request = new XMLHttpRequest();
-    request.open("GET", "/backend/status", false);
+    request.open("GET", "/backend/status", true);
+    request.onreadystatechange = function (e) {
+        if (request.readyState === 4 && request.status === 200) { 
+            var res = request.responseText;
+            console.log("hello: " + res);
+            output = JSON.parse(res);
+            if("token" in output && "username" in output) {
+                console.log(output.token);
+                console.log("ready");
+                var token_input = document.querySelector("#login > .token");
+                token_input.value = output.token;
+                var username_input = document.querySelector("#login > .username");
+                username_input.value = output.username;
+            }
+        }
+    };
+    request.onerror = function (e) {
+        console.error(request.statusText);
+    };
     request.send(null);
-    var res = request.responseText;
-    console.log("hello: " + res);
-    output = JSON.parse(res);
-    console.log(output.token);
 }
     
 
@@ -15,5 +29,4 @@ function pollBackend(output) {
 window.onload = function() {
     var output = { "status": "", "username": ""};
     setInterval( function() { pollBackend(output); }, 500 );
-    console.log(output);
 }
